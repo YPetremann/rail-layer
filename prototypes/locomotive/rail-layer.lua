00fcaccbc4ed4def367335abdf9497173d2a859c
Y.Petremann <y.petremann@gmail.com>
2025-09-29 00:16:43 +0200

    fix minable result for rail layer

--- prototypes/locomotive/rail-layer.lua
+++ prototypes/locomotive/rail-layer.lua
@@ -8,6 +8,7 @@ local rail_layer = structured_clone(data.raw["locomotive"]["locomotive"])
 rail_layer.name = "rail-layer"
 rail_layer.icon = "__rail-layer__/graphics/icons/rail-layer.png"
 rail_layer.color = {r=1,g=0.75,b=0}
+rail_layer.minable.result = "rail-layer"
 rail_layer.default_copy_color_from_train_stop=false
 rail_layer.allow_manual_color=false
 rail_layer.max_speed=0.25
